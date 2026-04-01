c0d38f57de801a8df560ad112de6cb5e1dbe8bff
Fadhilaaditya <fadhilaaditya@gmail.com>
2026-04-01 15:32:29 +0700

    new deploy

--- frontend/src/views/user/Homepage/_components/AboutSection.vue
+++ frontend/src/views/user/Homepage/_components/AboutSection.vue
@@ -4,17 +4,15 @@
 
 <template>
   <section id="about-us" class="py-16 px-6 md:px-12 bg-white relative overflow-hidden">
-<<<<<<< HEAD
-    <img src="/Kupu%20Kupu%201.svg" alt="Decoration Kupu Kupu" class="absolute -top-6 -right-6 w-32 md:w-64 md:top-5 md:right-0 pointer-events-none opacity-80" />
+    <!-- Decorative Icons (z-0 to stay behind text) -->
+    <img 
+      src="/Kupu%20Kupu%201.png" 
+      alt="Decoration Kupu Kupu" 
+      class="absolute bottom-10 -right-4 w-36 md:w-64 md:top-5 md:right-0 md:bottom-auto pointer-events-none opacity-40 md:opacity-90 z-0" 
+    />
 
-    <div class="absolute bottom-0 left-0 md:left-0 text-primary pointer-events-none opacity-80 md:opacity-100">
-       <img src="/6.%20EEV%20Lebaran%20-%20Flo%201.svg" alt="Decorative Flo" class="w-32 md:w-64" />
-=======
-    <img src="/Kupu%20Kupu%201.png" alt="Decoration Kupu Kupu" class="absolute top-5 right-0 w-28 md:w-50 pointer-events-none z-0" />
-
-    <div class="absolute bottom-0 left-0 text-primary pointer-events-none z-0">
-       <img src="/6.%20EEV%20Lebaran%20-%20Flo%201%20(1).png" alt="Decorative Flo" class="w-28 md:w-50"  />
->>>>>>> 399d507 (feat: add About Us, Call to Action, and FAQ sections to the user homepage with corresponding assets.)
+    <div class="absolute -bottom-8 -left-8 md:bottom-0 md:left-0 text-primary pointer-events-none opacity-40 md:opacity-100 z-0">
+       <img src="/6.%20EEV%20Lebaran%20-%20Flo%201%20(1).png" alt="Decorative Flo" class="w-40 md:w-64" />
     </div>
 
     <div class="max-w-4xl mx-auto mb-10 relative z-10">
